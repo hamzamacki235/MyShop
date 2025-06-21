454c36fb0c503966e3d14fa4c161c534afd0b96e
Hamza Maki <hamzamacki235@gmail.com>
2025-06-21 03:34:28 -0700

    First commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGJ4yTFdFsgcs46fb4Uy96O2i8Az1kbus",
+  authDomain: "electronicsmarket-95db5.firebaseapp.com",
+  projectId: "electronicsmarket-95db5",
+  storageBucket: "electronicsmarket-95db5.appspot.com",
+  messagingSenderId: "559298034101",
+  appId: "1:559298034101:web:df162b5a24bf974ede01fc",
+  measurementId: "G-G0G9ZHL5Z3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function toggleAddProduct() {
+  alert("نموذج إضافة المنتج سيظهر هنا!");
+}
+    
